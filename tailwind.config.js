5ac3d1cbde346a72de21fb348660c6f93db03ddb
bapidey <devbapidey@gmail.com>
2021-06-02 15:39:04 +0530

    tailwind with netninja at Jun 2 2021

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        my: ['Nunito'],
+      },
+      colors: {
+        primary: '#F0643F',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888888',
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
